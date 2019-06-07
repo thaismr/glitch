bda3e78c1099b38de16c1bd9e8b79997c6ba0e53
thaismr <thaismr@users.noreply.github.com>
2019-06-07 18:11:10 +0100

    very initial code

---
+++ client/components/TourCreate.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class TourCreate extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Create a new Tour:</h3>
+        <form>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default TourCreate
--- client/index.js
+++ client/index.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
 import ApolloClient from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
 
+import App from './components/App'
 import TourList from './components/TourList'
+import TourCreate from './components/TourCreate'
 
 const client = new ApolloClient({})
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <TourList />
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={TourList} />
+          <Route path="tour/new" component={TourCreate} />
+        </Route>
+      </Router>
     </ApolloProvider>
   )
 }
